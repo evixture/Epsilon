409fb32b91881f56661e89c842042522f42b1cad
pixture <pixture1311@gmail.com>
2019-12-10 22:40:14 -0600

    added weapon magazines to tileset and slight color changes

--- defines.h
+++ defines.h
@@ -10,37 +10,37 @@ constexpr auto PI = 3.14159265;
 
 /*
 	CHAR MAPPINGS
-	| 00 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 |
-
-	| 16 | 17 | 18 | 19 | 20 | 21 | ?? | ?? | 24 | 26 | 26 | 27 | ?? | 29 | 30 | 31 |
+	<	| 00 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 |
+	|TILES
+	|	| 16 | 17 | 18 | 19 | 20 | 21 | ?? | ?? | 24 | 26 | 26 | 27 | ?? | 29 | 30 | 31 |
+	|
+	<	| 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 |
 	
-	| 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 |
+	<	| 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 |
+	|ALPHA
+	|	| 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 |
+	|NUMBER
+	|	| 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 |
+	|SYMBOLS
+	|	| 96 | 97 | 98 | 99 | 100| 101| 102| 103| 104| 105| 106| 107| 108| 109| 110| 111|
+	|ETC
+	<	| 112| 113| 114| 115| 116| 117| 118| 119| 120| 121| 122| 123| 124| 125| 126| 127|
 	
-	| 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 |
+	<	| 128| 129| 130| 131| 132| 133| 134| 135| 136| 137| 138| 139| 140| 141| 142| 143|
+	|TOOLS
+	|	| 144| 145| 146| 147| 148| 149| 150| 151| 152| 153| 154| 155| 156| 157| 158| 159|
+	|ITEMS
+	<	| 160| 161| 162| 163| 164| 165| 166| 167| 168| 169| 170| 171| 172| 173| 174| 175|
 	
-	| 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 |
-	
-	| 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 |
-	
-	| 96 | 97 | 98 | 99 | 100| 101| 102| 103| 104| 105| 106| 107| 108| 109| 110| 111|
-	
-	| 112| 113| 114| 115| 116| 117| 118| 119| 120| 121| 122| 123| 124| 125| 126| 127|
-	
-	| 128| 129| 130| 131| 132| 133| 134| 135| 136| 137| 138| 139| 140| 141| 142| 143|
-	
-	| 144| 145| 146| 147| 148| 149| 150| 151| 152| 153| 154| 155| 156| 157| 158| 159|
-	
-	| 160| 161| 162| 163| 164| 165| 166| 167| 168| 169| 170| 171| 172| 173| 174| 175|
-	
-	| 176| 177| 178| 179| 180| 181| 182| 183| 184| 185| 186| 187| 188| 189| 190| 191|
-	
-	| 192| 193| 194| 195| 196| 197| 198| 199| 200| 201| 202| 203| 204| 205| 206| 207|
-	
-	| 208| 209| 210| 211| 212| 213| 214| 215| 216| 217| 218| 219| 220| 221| 222| 223|
-	
-	| 224| 225| 226| 227| 228| 229| 230| 231| 232| 233| 234| 235| 236| 237| 238| 239|
-	
-	|240 | 241| 242| 243| 244| 245| 246| 247| 248| 249| 250| 251| 252| 253| 254| 255|
+	<	| 176| 177| 178| 179| 180| 181| 182| 183| 184| 185| 186| 187| 188| 189| 190| 191|
+	|OTHER CHARS
+	|	| 192| 193| 194| 195| 196| 197| 198| 199| 200| 201| 202| 203| 204| 205| 206| 207|
+	|
+	|	| 208| 209| 210| 211| 212| 213| 214| 215| 216| 217| 218| 219| 220| 221| 222| 223|
+	|
+	|	| 224| 225| 226| 227| 228| 229| 230| 231| 232| 233| 234| 235| 236| 237| 238| 239|
+	|
+	<	| 240| 241| 242| 243| 244| 245| 246| 247| 248| 249| 250| 251| 252| 253| 254| 255|
 */
 
 #define CHAR_UpStair 16
@@ -49,6 +49,17 @@ constexpr auto PI = 3.14159265;
 #define CHAR_Table 20
 
 #define CHAR_Pistol 128
+#define CHAR_SMG 129
+#define CHAR_Rifle 130
+#define CHAR_DMR 131
+#define CHAR_Sniper 132
+
+#define CHAR_PistolMagazine 144
+#define CHAR_SMGMagazine 145
+#define CHAR_RifleMagazine 146
+#define CHAR_DMRMagazine 147
+#define CHAR_SniperMagazine 148
+
 
 /*
 	FLOOR HEIGHTS
@@ -183,23 +194,23 @@ ITEM SIZES
 //PISTOLS
 	//M1911
 		//WEAPON
-				#define TILE_M1911 std::make_shared<Tile>(CHAR_Pistol, TCODColor::copper, TCODColor::darkerGrey, 1, true, Tile::Tag::STATIC)
-				#define WEAPON_M1911 std::make_shared<Weapon>("M1911", TCODColor::grey, 7, 10, .15f, 1.0f, MagazineData::AmmoType::FOURTYFIVEACP, Weapon::FireType::SEMI)
+				#define TILE_M1911 std::make_shared<Tile>(CHAR_Pistol, TCODColor::darkerGrey, TCODColor::copper, 1, true, Tile::Tag::STATIC)
+				#define WEAPON_M1911 std::make_shared<Weapon>("M1911", TCODColor::darkerGrey, 7, 10, .15f, 1.0f, MagazineData::AmmoType::FOURTYFIVEACP, Weapon::FireType::SEMI)
 			#define ITEM_M1911(x, y, level) std::make_shared<Item>(2, TILE_M1911 , WEAPON_M1911, Position(x, y, level))
 		//MAGAZINE
-					#define TILE_45ACPMagazine7 std::make_shared<Tile>('/', TCODColor::darkerGrey, TCODColor::darkGrey, 1, true, Tile::Tag::STATIC)
-					#define TOOL_45ACPMagazine7 std::make_shared<Tool>("45 ACP Magazine -7-", TCODColor::darkGrey, '/')
+					#define TILE_45ACPMagazine7 std::make_shared<Tile>(CHAR_PistolMagazine, TCODColor::darkerGrey, TCODColor::copper, 1, true, Tile::Tag::STATIC)
+					#define TOOL_45ACPMagazine7 std::make_shared<Tool>("45 ACP Magazine -7-", TCODColor::darkerGrey, CHAR_PistolMagazine)
 				#define ITEM_45ACPMagazine7(x, y, level) Item(1, TILE_45ACPMagazine7 , TOOL_45ACPMagazine7, Position(x, y, level))
 			#define MAGAZINE_45ACPMagazine7(x, y, level) std::make_shared<MagazineItem>(ITEM_45ACPMagazine7(x, y, level), std::make_shared<MagazineData>(MagazineData::AmmoType::FOURTYFIVEACP, 7 , 7))
 
 //RIFLES
 	//M4A1
 		//WEAPON
-				#define TILE_M4A1 std::make_shared<Tile>(214, TCODColor::darkestGrey, TCODColor::darkGrey, 1, true, Tile::Tag::STATIC)
-				#define WEAPON_M4A1 std::make_shared<Weapon>("M4A1", TCODColor::darkestGrey, 30, 10, .09f, 2.0f, MagazineData::AmmoType::FIVEPOINTFIVESIX, Weapon::FireType::FULL)
+				#define TILE_M4A1 std::make_shared<Tile>(CHAR_Rifle, TCODColor::darkerGrey, TCODColor::copper, 1, true, Tile::Tag::STATIC)
+				#define WEAPON_M4A1 std::make_shared<Weapon>("M4A1", TCODColor::darkerGrey, 30, 10, .09f, 2.0f, MagazineData::AmmoType::FIVEPOINTFIVESIX, Weapon::FireType::FULL)
 			#define ITEM_M4A1(x, y, level) std::make_shared<Item>(3, TILE_M4A1 , WEAPON_M4A1, Position(x, y, level))
 		//MAGAZINE
-					#define TILE_556Magazine30 std::make_shared<Tile>('/', TCODColor::darkerGrey, TCODColor::darkGrey, 1, true, Tile::Tag::STATIC)
-					#define TOOL_556Magazine30 std::make_shared<Tool>("5.56 Magazine -30-", TCODColor::darkGrey, 'j')
+					#define TILE_556Magazine30 std::make_shared<Tile>(CHAR_RifleMagazine, TCODColor::darkerGrey, TCODColor::copper, 1, true, Tile::Tag::STATIC)
+					#define TOOL_556Magazine30 std::make_shared<Tool>("5.56 Magazine -30-", TCODColor::darkerGrey, CHAR_RifleMagazine)
 				#define ITEM_556Magazine30(x, y, level) Item(1, TILE_556Magazine30 , TOOL_556Magazine30, Position(x, y, level))
 			#define MAGAZINE_556Magazine30(x, y, level) std::make_shared<MagazineItem>(ITEM_556Magazine30(x, y, level), std::make_shared<MagazineData>(MagazineData::AmmoType::FIVEPOINTFIVESIX, 30 , 30))
